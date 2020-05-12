7d8b635dda3b2c997ab87c480c24f8f1fd81875c
floriansr <sueurfloriansf@gmail.com>
2020-05-12 23:34:37 +0200

    Translation configurated with useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React, { useState, useEffect } from "react";
-import Example from "components/Example";
+import React, { useState } from "react";
+import { IntlProvider } from "react-intl";
+
+import fr from "assets/translation/fr";
+import en from "assets/translation/en";
+import LanguagesContext from "context/LanguagesContext";
+
+import Home from "components/Home";
+
+const messages = {
+	fr,
+	en,
+};
 
 const App = () => {
-	const [data, setData] = useState("");
-
-	useEffect(() => {
-		setData();
-	}, []);
+	const [language, setLanguage] = useState("fr");
 
 	return (
 		<>
-			<Example name={data} />
+			<IntlProvider locale={language} messages={messages[language]}>
+				<LanguagesContext.Provider value={{ setLanguage }}>
+					<Home />
+				</LanguagesContext.Provider>
+			</IntlProvider>
 		</>
 	);
 };
